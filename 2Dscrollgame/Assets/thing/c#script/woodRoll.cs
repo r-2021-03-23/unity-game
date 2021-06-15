6b5f4d100f24538046cc7a0ae96e54512a5d172f
otis890102 <otis890102@yahoo.com.tw>
2021-06-15 13:19:24 +0800

    0615

---
+++ 2Dscrollgame/Assets/thing/c#script/woodRoll.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class woodRoll : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce()
+    }
+}
