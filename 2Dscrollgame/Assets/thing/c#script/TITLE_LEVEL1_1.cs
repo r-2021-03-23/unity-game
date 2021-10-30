56cc4c2736530c3bd9df409e245c1fb1d5f6f843
otis890102 <otis890102@yahoo.com.tw>
2021-10-30 20:52:33 +0800

    1030

--- 2Dscrollgame/Assets/thing/c#script/TITLE_LEVEL1_1.cs
+++ 2Dscrollgame/Assets/thing/c#script/TITLE_LEVEL1_1.cs
@@ -33,4 +33,39 @@ public class TITLE_LEVEL1_1: MonoBehaviour
         SceneManager.LoadScene("game-level1-simple");
     }
 
+    public void level1_2()
+    {
+        SceneManager.LoadScene("game-level1-normal");
+    }
+
+    public void level1_3()
+    {
+        SceneManager.LoadScene("game-level1-hard");
+    }
+
+    public void level2_1()
+    {
+        SceneManager.LoadScene("game-level1-simple");
+    }
+
+    public void level2_2()
+    {
+        SceneManager.LoadScene("game-level2-normal");
+    }
+    public void level2_3()
+    {
+        SceneManager.LoadScene("game-level2-hard");
+    }
+    public void level3_1()
+    {
+        SceneManager.LoadScene("game-level2-simple");
+    }
+    public void level3_2()
+    {
+        SceneManager.LoadScene("game-level3-normal");
+    }
+    public void level3_3()
+    {
+        SceneManager.LoadScene("game-level1-hard");
+    }
 }
