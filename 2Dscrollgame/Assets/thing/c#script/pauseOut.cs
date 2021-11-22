afbd3af355167b46455c3d999f3f9ed0eb74810d
otis890102 <a7251494@ulive.pccu.edu.tw>
2021-11-22 14:53:16 +0800

    1122 2

---
+++ 2Dscrollgame/Assets/thing/c#script/pauseOut.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseOut: MonoBehaviour
+{
+    public GameObject pause,pauseMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.JoystickButton9))
+        {
+            pauseMenu.SetActive(true);
+            pause.SetActive(false);
+        }
+    }
+}
