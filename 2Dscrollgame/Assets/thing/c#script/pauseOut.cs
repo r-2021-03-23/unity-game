d9f622146e3328abbf7009a30f5d95b83ca41860
otis890102 <a7251494@ulive.pccu.edu.tw>
2021-11-22 22:22:02 +0800

    1122 3

--- 2Dscrollgame/Assets/thing/c#script/pauseOut.cs
+++ 2Dscrollgame/Assets/thing/c#script/pauseOut.cs
@@ -1,25 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class pauseOut: MonoBehaviour
 {
     public GameObject pause,pauseMenu;
     
+    public static bool isGamePaused = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pauseMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isGamePaused == true)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
         if(Input.GetKeyDown(KeyCode.JoystickButton9))
         {
-            pauseMenu.SetActive(true);
-            pause.SetActive(false);
+            if(isGamePaused)
+            {
+                clickPaused();
+                Time.timeScale = 0f;  
+            }
+            else
+            {
+                clickResume();
+                Time.timeScale = 1f;  
+            }
         }
+    }
+
+     public void clickPaused()
+    {   
+        pauseMenu.SetActive(true);
+        pause.SetActive(false);
+        isGamePaused = true;
+    }
+
+    public void clickResume()
+    {
+        pauseMenu.SetActive(false);
+        pause.SetActive(true);
+        isGamePaused = false;  
+    }
+    public void clickMenu()
+    {
+        SceneManager.LoadScene("gameStart");
     }
 }
